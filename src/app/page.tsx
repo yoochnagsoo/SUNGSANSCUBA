2a634e6e7a39bae9cde2b7ea977c2646e8f6dbcb
yoochnagsoo <yoocs0@gmail.com>
2026-07-08 22:21:41 +0900

    feat: add quick reservation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,13 @@ import Gallery from "@/components/home/Gallery";
 import Reviews from "@/components/home/Reviews";
 import ReservationCTA from "@/components/home/ReservationCTA";
 import InstagramSection from "@/components/home/InstagramSection";
+import QuickReservationBar from "@/components/home/QuickReservationBar";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <QuickReservationBar />
        <AboutSection />
        <WhyChooseUs />
        <DiveDestinations />
